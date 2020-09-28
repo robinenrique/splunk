7d10002a3a099b5025ad15ef6eeea550923ede8a
Robin Gonzalez <robingonzalez@tests-iMac.local>
2020-09-27 18:50:55 -0700

    Cleaning code

--- last_logins.py
+++ last_logins.py
@@ -5,55 +5,63 @@ import splunklib.results as results
 import splunklib.client as client
 import config
 
-conf = config.auth
-user = conf['username']
-passw = conf['password']
-port = conf['port']
-host = conf['host']
+# config.py should be populated with remote client or localhost, port 8089 and valid credentials
+def get_config():
+    conf = config.auth
+    user = conf['username']
+    passw = conf['password']
+    port = conf['port']
+    host = conf['host']
+    return [host, port, user, passw]
 
+# start connection
+host, port, user, passw = get_config()
 service = client.connect(host=host, port=port, username=user, password=passw)
 
-# obtained this template from Splunk's dev documentation
-# <source>: https://dev.splunk.com/enterprise/docs/devtools/python/sdk-python/howtousesplunkpython/howtorunsearchespython
+# used dev documentation found on: https://dev.splunk.com/enterprise/docs/devtools/python/sdk-python/howtousesplunkpython
 def get_audit_logs():
-    kwargs_export = {"earliest_time": "-7d",
+    # setup search interval
+    kwargs_export = {"earliest_time": "-7d", # 1 week from now
                      "latest_time": "now",
                      "search_mode": "normal"}
-    searchquery_export = "search index=_audit"
 
-    exportsearch_results = service.jobs.export(searchquery_export, **kwargs_export)
+    # search in the _audit index
+    query = "search index=_audit"
+    exportsearch_results = service.jobs.export(query, **kwargs_export)
 
-    # Get the results and display them using the ResultsReader
+    # display results
     reader = results.ResultsReader(exportsearch_results)
     for result in reader:
         if isinstance(result, dict):
             yield result['_raw']
-        elif isinstance(result, results.Message):
-            # Diagnostic messages may be returned in the results
-            # Not sure what to do with these messages... look like error messages
-            continue
 
+# crawls the _raw values in logs
 def crawl_login_attempts():
     for log in get_audit_logs():
-        # I am sorry for the weird parsing... this could definitely be improved with find() calls
+        # getting rid of weird chars, this could be improved with a regex or better string patterns
         log = log.replace("Audit:", "").replace("[","").replace("]","").split(",")
         action = log[2].replace(" ", "")
+        
+        # login - check if successful or failed
         if action == "action=loginattempt":
+            # some more cleaning that can be improved
             timestamp, user = log[0], log[1].replace(" ", "")
-            # logs are in a weird format, so need to do some cleaning to get the status of login
             status_idx = log[3].find("info=")
             ip_idx = log[3].find("clientip=")
+
             status, client_ip = "", ""
             if status_idx != -1:
                 status = log[3][status_idx:].split(" ")[0]
             if ip_idx != -1:
                 client_ip = log[3][ip_idx:].split(" ")[0]
             else:
-                # clientip can also be called src in some logs
+                # started running into some edge cases e.g.: clientip can also be called src in some logs
+                # this is not covering 100% of the logs atm
                 if len(log) > 4:
 	            ip_idx = log[4].find("src=")
         	    if ip_idx != -1:
                	        client_ip = log[4][ip_idx:].split(" ")[0]
+            # yield msg
             yield timestamp + " " + user + " " + status.replace("info=", "status=") + " " + client_ip.replace("clientip=", "src=")
 
 def main():
